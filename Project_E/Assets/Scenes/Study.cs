e2d8581197e78327a44c4dc2cf917ea71c31300c
sgjun0928 <sgjun0928@gmail.com>
2025-11-26 16:55:45 +0900

    [Add] 251126_NewProject_3dObject

---
+++ Project_E/Assets/Scenes/Study.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Study : MonoBehaviour
+{
+    // 이게 주석입니다.//
+    /* 이렇게도
+     * 쓸 수 있지롱
+     * 하하하 */
+    
+   
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
